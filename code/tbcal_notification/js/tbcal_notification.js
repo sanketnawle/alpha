4fe0d8911359b3c23fbbe6538dc81aea44f1655d
jakelaz <JacobLazarus@172-26-33-46.dynapool.nyu.edu>
2014-06-11 09:18:46 -0400

    commmit

--- code/tbcal_notification/js/tbcal_notification.js
+++ code/tbcal_notification/js/tbcal_notification.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-
+	$(document).delegate(".past_events_tab","click",function(){
+		$(this).closest(".tabs").find(".active_pe_tab").removeClass("active_pe_tab");
+		if(!$(this).hasClass("active_pe_tab")){
+			$(this).find(".little-glyph").addClass("active_glyph");
+			$(this).addClass("active_pe_tab");
+			$(".past_events_tab").css("border-right-color","rgba(87, 87, 87, 0.7)");
+		}
+	});
+	$(document).delegate(".active_pe_tab","click",function(){
+			$(this).removeClass("active_pe_tab");
+			$(this).find(".little-glyph").removeClass("active_glyph");
+			$(".past_events_tab").css("border-right-color","#e9eaed");
+		});
 });   
