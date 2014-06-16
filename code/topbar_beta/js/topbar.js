245bb22aab4a16f0bdcc1bfd9acbfd51fdc6fd7b
Kuan Wang <K@172-26-206-184.dynapool.nyu.edu>
2014-06-16 11:40:39 -0400

    top bar update

--- code/topbar_beta/js/topbar.js
+++ code/topbar_beta/js/topbar.js
@@ -4,47 +4,12 @@ $(function() {
 	var d = new Date();
 	var t= d.getDate();              
 	var nm= "'src/calendar-icons/"+t+".png'";
-
-
 	$(".cal_icon").attr("src","src/calendar-icons/"+t+".png");
 
 
-	var pw_final=740;
-	var ban_aug=0;
-	$( window ).resize(function() {
-		$(".topbar_search_input").removeClass("augged");
-		$(".topbar_search_input").css({"width":"300px"});
 
-
-		if($(window).width()<=1080){
-			ban_aug=1;
-		}else{
-			ban_aug=0;
-		}
-		if($(window).width()<=740){
-			
-		$( ".topbar_search_input" ).css( "width", ( $( window ).width() * 0.4 | 0 ) + "px" );
-		
-
-		if($( ".topbar_search_input" ).width()<=268){$( ".topbar_search_input" ).css( "width", 220 + "px" );}
-		}else{
-		
-		$( ".topbar_search_input" ).css( "width", 300 + "px" );	
-		}
-	});
 
 	$( document ).delegate( ".topbar_search_input", "click", function() {
-		//$(this).css({"background-color":"white"});
-		if(ban_aug==0){
-			if(!$(this).hasClass("augged")){
-			$(this).addClass("augged");
-			$(this).css({"width":"430px"});
-			$(".topbar_qicon").hide();
-
-			}
-			
-		}
-		
 		//$(".graph_search").show();
 	});
 
@@ -74,7 +39,6 @@ $(function() {
 			     	if(!$container.is($target)&&($container.has($target).length===0)){
 			     		$(".topbar_qicon").show();
 			     		var w= $(".topbar_search_input").width();
-			     		if(w>=300){w=300;}
 
 			     		$(".topbar_search_input").removeClass("augged");
 						$(".topbar_search_input").css({"width":w});
@@ -98,5 +62,14 @@ $(document).delegate(".topbar_qicon img","mouseout",function(){
 $(document).delegate(".card-tag","mouseout",function(){
 	$(this).delay(1).hide(0);
 });
+
+$(document).delegate(".topbar_left","click",function(){
+	if($(this).find(".leftbar_close").hasClass("flip")){
+	$(this).find(".leftbar_close").removeClass("flip");
+	}else{
+	$(this).find(".leftbar_close").addClass("flip");	
+	}
+});
+
 
 });
