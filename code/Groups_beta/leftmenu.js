d22a8fb2de18e5f0b7b302158dcf419162c94ea0
Jakelaz <jacobmlazarus@gmail.com>
2014-06-17 09:54:00 -0400

    groups_chainges

--- code/Groups_beta/leftmenu.js
+++ code/Groups_beta/leftmenu.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
 		if(!$(".burger").hasClass("flipBurger")){
 			$(".burger").addClass("flipBurger");
 			$(".leftmenu").animate({ opacity: "0", marginLeft: "-300px"},200);
+			$(".mid_right_sec").css({"float":"none","padding-left":"0px"});
 		}
 		else{
 			$(".burger").removeClass("flipBurger");
 			$(".leftmenu").animate({ opacity: "1", marginLeft: "0px"},200);
+			$(".mid_right_sec").css({"float":"left","padding-left":"318px"});
 		}
 	});
 });
