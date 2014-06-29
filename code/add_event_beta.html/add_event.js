559e4335fe032de541ddba08f42a1bca1eb217f5
Jakelaz <jacobmlazarus@gmail.com>
2014-06-29 03:05:41 -0400

    the add event invite experience

--- code/add_event_beta.html/add_event.js
+++ code/add_event_beta.html/add_event.js
@@ -27,5 +27,24 @@ $(document).ready(function() {
 		$(".down-arrow-2").hide();
 		$(".add-endtime").show();
 	});
+	$(document).delegate('.event-invites',"mousedown", function(){
+		$(this).focus();
+		$(".add-event-dd-box-invite").show();
+	});
 
+	$(document).delegate('.ddbox-invite-option',"click", function(){
+		$(this).addClass("ddbox-option-invited");
+		$(this).find(".after-click-effect").show();
+		$(this).find(".invite-option-checkwrap").css({"background-image":"url(src/checked-invite.png)","opacity":"1"});
+	});
+
+	$(document).delegate('.close-invite-ddbox',"click", function(){
+		$(".add-event-dd-box-invite").hide();
+	});
+
+	$(document).delegate('.ddbox-option-invited',"click", function(){
+		$(this).removeClass("ddbox-option-invited");
+		$(this).find(".after-click-effect").hide();
+		$(this).find(".invite-option-checkwrap").css({"background-image":"url(src/unchecked-invite.png)","opacity":".7"});
+	});
 });
