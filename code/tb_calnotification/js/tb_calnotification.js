f521571b9fedaa39fe5d30797ebda718adcb4927
Kuan Wang <K@172-26-206-63.dynapool.nyu.edu>
2014-07-02 09:48:40 -0400

    archive calnoti

--- code/tb_calnotification/js/tb_calnotification.js
+++ code/tb_calnotification/js/tb_calnotification.js
@@ -31,5 +31,26 @@ $(document).delegate(".h_remove_icon","mouseout",function(){
 	$(this).css({"background-image":"url(img/hide.png)"});
 });	
 
+	$(document).delegate(".past_events_tab","click",function(){
+		$(this).closest(".tabs").find(".active_pe_tab").removeClass("active_pe_tab");
+		if(!$(this).hasClass("active_pe_tab")){
+			$(this).find(".little-glyph").addClass("active_glyph");
+			$(this).addClass("active_pe_tab");
+			$(".past_events_tab").css("border-right-color","rgba(87, 87, 87, 0.7)");
+		}
+	});
+	$(document).delegate(".active_pe_tab","click",function(){
+			$(this).removeClass("active_pe_tab");
+			$(this).find(".little-glyph").removeClass("active_glyph");
+			$(".past_events_tab").css("border-right-color","#e9eaed");
+		});
+
+	$(document).delegate(".cal_noti_see","click",function(){
+		$(".active_pe_tab").removeClass("active_pe_tab");
+		$(".little-glyph").removeClass("active_glyph");
+		$(".past_events_tab").css("border-right-color","#e9eaed");
+	});
+
+
 
 });   
