348e67bf0625d372c17440ade3a13d8da7f94cb9
Alex <adroitcode>
2014-10-02 14:16:48 -0400

    lol

--- urlinqyii/index.php
+++ urlinqyii/index.php
@@ -1,7 +1,11 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../yii/framework/yii.php';
+
+
+//Edit this directory for yii 
+
+$yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
