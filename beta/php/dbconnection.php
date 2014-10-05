b4ee404831cf6d1ca54384911bc9ab098645bbc5
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2014-10-05 16:38:44 -0400

    search bar updated

--- beta/php/dbconnection.php
+++ beta/php/dbconnection.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "campusla_UrlinqU";
-$password = "mArCh3!!1992X";
-$database = "campusla_urlinq_beta";
+$user = "root";
+$password = "root";
+$database = "urlinq_beta";
 // if(isset($con))
 // $con->close();
 $con = mysqli_connect($host, $user, $password, $database);
