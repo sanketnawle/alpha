032f3e945c12fde391ddde7fa11dd80bddd4604c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2014-10-10 11:37:51 -0400

    finished searchbar
    but yet to fine tune it

--- beta/leftpanel_beta/js/lptopbar.js
+++ beta/leftpanel_beta/js/lptopbar.js
@@ -1,6 +1,6 @@
 ﻿(function ($) {
     var leftPanelOpen = true;
-    var openNotify = null;
+    openNotify = null;
     var actionTitle = "";
 
     var weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -23,13 +23,58 @@
         }).on("keyup", function (e) {
             e = e || event;
             var text = $(this);
+            var postlist = text.closest(".center").find(".postlist");
             if (e.keyCode == 27) {
                 text.val("");
                 text.trigger("blur");
             } else if (text.val().trim() == "") {
-                text.closest(".center").find(".postlist").hide();
+                postlist.hide();
                 text.closest(".center").find(".prelist").show();
             } else {
+                var query = text.val().trim();
+                $.ajax({
+                    url: "lptopbar_search.php",
+                    data: {
+                        q: query,
+                        ajax: true
+                    },
+                    success: function (result) {                        
+                        result = eval(result);
+                        postlist.html("");
+                        result.forEach(function (e) {
+                            var link = $("<a />");
+                            var href = "../"; // change to ./ once you are in root
+                            var cls = "src";
+                            switch (e.type) {
+                                case "pro":
+                                case "stu":
+                                    href += "profile.php?user_id=";
+                                    cls = "pro";
+                                    break;
+                                case "cls":
+                                    href += "courses.php?user_id=";
+                                    cls = "cls";
+                                    break;
+                                case "dpt":
+                                    href += "department.php?user_id=";
+                                    cls = "dpt";
+                                    break;
+                                case "clb":
+                                    href += "clubs.php?user_id=";
+                                    cls = "clb";
+                                    break;
+                            }                            
+                            link.attr("href", href + e.id);
+
+                            var disp = e.name.replace(new RegExp("(" + query + ")", "ig"), "<b>$1</b>");
+
+                            link.append($("<div class='icon " + cls + "'><div>")).append("<span>" + disp + "</span>");
+                            postlist.append($("<li />").append(link));
+                        });
+                        postlist.append("<li><a href='search_beta.php?query=" + query + "'>" +
+                            "Seee all results for \"" + query + "\"</a></li>");
+                    }
+                })
                 text.closest(".center").find(".postlist").show();
                 text.closest(".center").find(".prelist").hide();
             }
@@ -124,40 +169,43 @@
                     '<div class="ttcontent">' + actionTitle + '</div>' +
                 '</div>');
             var icon = $(this);
-            nottooltip.css({ "top": icon.offset().top, "left": icon.offset().left + 22 });
+            nottooltip.css({ "top": icon.offset().top, "left": icon.offset().left + icon.outerWidth() });
             $("body").append(nottooltip);
         }, function () {
             $(this).attr("title", actionTitle);
             $("div.nottooltip").remove();
         });
-        $(".topbar .notify .button").click(function () {
-            // Reset
-            $(".topbar .notify").removeClass("active");
-            $(".notify-window").hide();
-
-            var notify = $(this).parents(".notify");
+        $(".topbar .notify").mouseenter(function (e) {
+            var notify = $(this);//.parents(".notify");
             if (openNotify !== notify[0]) {
-                console.log("I am in", openNotify, notify);
-                notify.toggleClass("active");
+                var window = null;
+                $(".topbar .notify").removeClass("active");
+                $(".topbar .notify-window").hide();
+                notify.addClass("active");
+                if (notify.hasClass("calendar")) window = $(".topbar .notify-window.calendar");
+                else if (notify.hasClass("board")) window = $(".topbar .notify-window.board");
 
-                if (notify.hasClass("calendar")) {
-                    $(".notify-window.calendar").toggle();
-                } else if (notify.hasClass("board")) {
-                    $(".notify-window.board").toggle();
-                }
+                if (openNotify === null) window.slideDown(150);
+                else window.show();
+
                 openNotify = notify[0];
-            } else {
-                openNotify = null;
             }
         })
+        $(".topbar .right").mouseleave(function () {
+            $(".topbar .notify").removeClass("active");
+            $(".topbar .notify-window").slideUp(150);
+            openNotify = null;
+        })
+
         $(".notify-window .toolbar li").click(function () {
             $(".notify-window .toolbar li").removeClass("active");
             $(this).addClass("active");
             var col = $(this).data("column");
-            var window = $(this).parents(".notify-window");
-            console.log(col, window);
-            $(".column", window).hide();
-            $(".column." + col, window).show();
+            if (col) {
+                var window = $(this).parents(".notify-window");
+                $(".column", window).hide();
+                $(".column." + col, window).show();
+            }
         })
         $(window).resize();
     });
