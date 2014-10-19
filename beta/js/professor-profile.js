4eb1f01f32bcbad4e5d85774c744fe22719aab29
Kuan Wang <K@dhcp-10-5-138-178.wireless.rochester.edu>
2014-10-19 14:56:24 -0400

    profile update

--- beta/js/professor-profile.js
+++ beta/js/professor-profile.js
@@ -2111,8 +2111,8 @@ $(document).ready(function () {
 
     function check_char_lim(del){
         var strl= del.val().length;
-        $(".char_reminder").find("span").text(strl);
-        if (strl>=(txt_lmt-txt_lmt/6)) {
+        $(".char_reminder").find("span").text(txt_lmt-strl);
+        if (strl>=280) {
             $(".char_reminder").addClass("about_to_exceed");
         }else{
             $(".char_reminder").removeClass("about_to_exceed");
@@ -2121,6 +2121,29 @@ $(document).ready(function () {
 
 
     /*char counter end*/
+
+    //textara auto growth
+    $(".autogrowth_textarea").mousemove(function(e) {
+        var myPos = $(this).offset();
+        myPos.bottom = $(this).offset().top + $(this).outerHeight();
+        myPos.right = $(this).offset().left + $(this).outerWidth();
+        
+        if (myPos.bottom > e.pageY && e.pageY > myPos.bottom - 16 && myPos.right > e.pageX && e.pageX > myPos.right - 16) {
+            $(this).css({ cursor: "nw-resize" });
+        }
+        else {
+            $(this).css({ cursor: "" });
+        }
+    })
+    //  the following simple make the textbox "Auto-Expand" as it is typed in
+    .keyup(function(e) {
+        //  the following will help the text expand as typing takes place
+        while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height()+1);
+        };
+    });
+    //textara auto growth end
+
 
     /* Code to manage the dropdowns */
 
