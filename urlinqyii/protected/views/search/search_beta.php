12b6f04affe22a20deb09c841284404b8166cc39
Gary <garysapozhn@gmail.com>
2014-10-21 12:53:11 -0400

    srch

---
+++ urlinqyii/protected/views/search/search_beta.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MikeS
+ * Date: 10/21/14
+ * Time: 12:47 PM
+ */
+
+class search_beta {
+
+} 
---
+++ urlinqyii/protected/views/search/search_filter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MikeS
+ * Date: 10/21/14
+ * Time: 12:48 PM
+ */ 
