51d89091a88de759413fcbd388f44dabea9ad1b4
Gary <garysapozhn@gmail.com>
2014-11-12 15:45:15 -0500

    srch

--- urlinqyii/protected/config/main.php
+++ urlinqyii/protected/config/main.php
@@ -6,38 +6,38 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+    'name'=>'My Web Application',
 //    'timeZone' => 'UTC',
-	// preloading 'log' component
-	'preload'=>array('log'),
+    // preloading 'log' component
+    'preload'=>array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
+    // autoloading model and component classes
+    'import'=>array(
+        'application.models.*',
+        'application.components.*',
+    ),
 
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		//*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'test',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		//*/
-	),
+    'modules'=>array(
+        // uncomment the following to enable the Gii tool
+        //*
+        'gii'=>array(
+            'class'=>'system.gii.GiiModule',
+            'password'=>'test',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            'ipFilters'=>array('127.0.0.1','::1'),
+        ),
+        //*/
+    ),
 
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		// uncomment the following to enable URLs in path-format
-		//*
+    // application components
+    'components'=>array(
+        'user'=>array(
+            // enable cookie-based authentication
+            'allowAutoLogin'=>true,
+        ),
+        // uncomment the following to enable URLs in path-format
+        //*
 //		'urlManager'=>array(
 //            'urlFormat'=>'path',
 //            'showScriptName'=>false,
@@ -76,10 +76,11 @@ return array(
                 //'club/<id:\d+>/members'=>'club/members',
                 //'club/<id:\d+>/member/remove'=>'club/removeMember',
 
-                'search/<q>' => 'search/json',
+                //'search/<q>' => 'search/json', //going to need to redirect to search/view, by passing the JSON there
                 //urlinq.com/getposts
                 'getposts'=>'profile/getPosts',
                 'search/'=>'search/view',
+                'search/results'=>'search/results',
 
                 'file/upload'=>'site/fileUpload',
 
@@ -92,45 +93,45 @@ return array(
 
             ),
         ),
-		//*/
+        //*/
 //		'db'=>array(
 //			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //		),
-		// uncomment the following to use a MySQL database
+        // uncomment the following to use a MySQL database
 
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=urlinq_new',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => 'root',
-			'charset' => 'utf8',
-		),
+        'db'=>array(
+            'connectionString' => 'mysql:host=localhost;dbname=urlinq_new',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
 
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
+        'errorHandler'=>array(
+            // use 'site/error' action to display errors
+            'errorAction'=>'site/error',
+        ),
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                array(
+                    'class'=>'CWebLogRoute',
+                ),
+                */
+            ),
+        ),
+    ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'=>array(
+        // this is used in contact page
+        'adminEmail'=>'webmaster@example.com',
+    ),
 );
