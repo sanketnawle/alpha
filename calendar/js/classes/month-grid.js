84a191c2721a2ed8af8eeec3acc80f9fb70d5a06
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2014-11-12 11:41:51 -0500

    Updated Calendar
    So far

--- calendar/js/classes/month-grid.js
+++ calendar/js/classes/month-grid.js
@@ -4,34 +4,121 @@
 var MonthGrid = (function (MonthGrid) {
     var dp = new DateProvider();
 
-    var GridItem = function (text, outOfRange) {
+    var createGridItem = function (text, oor) {
         var item = document.createElement("div");
+        var date = document.createElement("div");
+        date.innerHTML = text;
+        date.className = "date";
         item.className = "grid-item";
-        if(outOfRange) item.classList.add("out-of-range")
-        item.innerHTML = text;
+        if (oor) item.classList.add("out-of-range")
+        item.appendChild(date);
         return item;
     }
 
-    var Grid = function (grid, month, year) {        
-        var start = dp.getDay(month, year, false);
-        var end = dp.getDate(month, year, true).getDate();
-
-        grid.innerHTML = "";
-
-        for (var i = 1 - start; i <= 42 - start; ++i) {
-            var text = "";
-            if (i < 1) text += dp.getMonthName(month == 0 ? 11 : month - 1, true) + " ";
-            else if (i > end) text += dp.getMonthName((month + 1) % 12, true) + " ";
-            text += new Date(year, month, i).getDate();
-            grid.appendChild(new GridItem(text), i < 1 || i > end);
-        }
+    var getGridItems = function (itemNodes) {
+        var items = [];
+        [].forEach.call(itemNodes, function (ele, i, array) { items.push(new GridItem(ele)); });
+        return items;
     }
 
-    MonthGrid.createGrid = function (elementId, month, year) {
-        if (month === undefined) month = new Date().getMonth();
-        if (year === undefined) year = new Date().getFullYear();
+    /* Class GridItem */
+    var GridItem = function (element, text, oor) {
+        this.ele = element || createGridItem(text, oor);
+        Object.defineProperties(this, {
+            addEvent: {
+                value: function (text) {
+                    var event = document.createElement("div");
+                    event.className = "grid-event";
+                    event.innerHTML = text || "Test Event"
+                    this.ele.appendChild(event);
+                }
+            }
+        })
+    }
 
-        return new Grid(document.getElementById(elementId), month, year);
+    /* Class Grid */
+    var Grid = function (grid, month, year) {
+        Object.defineProperties(this, {
+            ele: { value: grid },
+            populate: {
+                value: function () {
+                    var premature = false;
+                    var prem_plus = false;
+                    var start = dp.getDate(month, year, false).getDay();
+                    var end = dp.getDate(month, year, true).getDate();
+
+                    this.ele.innerHTML = "";
+
+                    for (var i = 1 - start, text = "", gc = 1; i <= 42 - start; ++i, text = "", gc += 1) {
+                        if (i < 1) text = dp.getMonthName(month == 0 ? 11 : month - 1, true);
+                        else if (i > end) text = dp.getMonthName((month + 1) % 12, true);
+                        text += " " + new Date(year, month, i).getDate();
+
+                        var gi = new GridItem(null, text, i < 1 || i > end).ele;
+
+                        // find today                        
+                        if (dp.getCurrentMonth() == month &&
+                            dp.getCurrentYear() == year &&
+                            dp.getCurrentDate() == i) {
+                            gi.classList.add("td");
+                        }
+
+                        this.ele.appendChild(gi);
+
+                        if (gc % 7 == 0 && gc / 7 < 6 && i >= end) {
+                            if (i == end && i == 28) prem_plus = true;
+                            premature = true; break;
+                        }
+                    }
+
+                    // customization
+                    if (premature) {
+                        var cls = prem_plus ? "prem-plus" : "prem";
+                        for (i = 0; i < this.items.length; ++i) {
+                            this.items[i].ele.classList.add(cls);
+                        }
+                    }
+
+                    var diff = new Date().getWeek() - dp.getDate(month, year, false).getWeek();
+                    var factor = premature ? prem_plus ? 4 : 5 : 6;
+                    for (i = 0; i < this.items.length; ++i) {
+                        if ((diff > 0 && diff < factor) || (diff == 0 && new Date().getDate() < 7))
+                            if (Math.floor(i / 7) == diff) this.items[i].ele.classList.add("tl");
+                        if (Math.floor(i / 7) + 1 == factor) this.items[i].ele.classList.add("ll");
+                    }
+                }
+            },
+            children: { get: function () { return this.ele.querySelectorAll(".grid-item"); } },
+            items: { get: function () { return getGridItems(this.children); } },
+        });
+
+        // intialize
+        this.populate();
+    }
+
+    MonthGrid.Grid = Grid;
+    MonthGrid.GridItem = GridItem;
+
+    MonthGrid.createGrid = function (object, month, year, isclass) {
+        if (isclass === undefined) isclass = false;
+
+        if (month === undefined) month = dp.getCurrentMonth();
+        if (year === undefined) year = dp.getCurrentYear();
+
+        var grid = [];
+        var ele = typeof object == "string" ? (isclass ?
+            document.getElementsByClassName(object) :
+            document.getElementById(object)) :
+            object;
+        if (ele === null || ele === undefined) return null;
+
+        if (isclass) {
+            for (var i = 0; i < ele.length; grid.push(new Grid(ele[i], (month + i) % 12, year)), ++i);
+        } else {
+            grid = new Grid(ele, month, year);
+        }
+
+        return grid;
     }
 
     return MonthGrid;
---
+++ calendar/js/extensions/printer.js
@@ -0,0 +1,28 @@
+﻿function outerHTML(node, cls) {
+    var div = document.createElement('div'), h, printable = node.cloneNode(true);
+    printable.classList.add(cls || "");
+    div.appendChild(printable);
+    h = div.innerHTML;
+    div = null;
+    return h;
+}
+
+ulcal.service("PrinterService", function ($timeout) {
+    this.print = function (node) {
+        var mywindow = window.open('', 'my div', 'height=695,width=900');
+        mywindow.document.write('<html><head><title>my div</title>');
+        mywindow.document.write('<link href="http://goo.gl/CHQFJX" rel="stylesheet" type="text/css">');
+        mywindow.document.write('<link href="font/avenir.css" rel="stylesheet" type="text/css">');
+        mywindow.document.write('<link rel="stylesheet" href="css/master.css" type="text/css" />');
+        mywindow.document.write('<link rel="stylesheet" href="css/week.css" type="text/css" />');
+        mywindow.document.write('<link rel="stylesheet" href="css/month.css" type="text/css" />');
+        mywindow.document.write('<link rel="stylesheet" href="css/header.css" type="text/css" />');
+        mywindow.document.write('</head><body class="ul-calendar print">');
+        mywindow.document.write(outerHTML(node, "print"));
+        mywindow.document.write('</body></html>');
+        $timeout(function () {
+            mywindow.print();
+            mywindow.close();
+        }, 1000);
+    }
+});
