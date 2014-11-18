809fdae57304d0dba9f96fad93e8a9c6a99c4b89
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2014-11-18 12:31:55 -0500

    another

--- urlinqyii/protected/config/db_config.php
+++ urlinqyii/protected/config/db_config.php
@@ -6,7 +6,7 @@ function get_db_array(){
         'connectionString' => 'mysql:host=localhost;dbname=urlinq_new',
         'emulatePrepare' => true,
         'username' => 'root',
-        'password' => 'root',
+        'password' => '',
         'charset' => 'utf8',
     );
 }
