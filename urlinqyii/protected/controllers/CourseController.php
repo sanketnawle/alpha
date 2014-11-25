a1b2e6cc5ab2181e56bc5ee0e954dc0e0f3d5547
abrar_sheikh <abrar2002as@gmail.com>
2014-11-25 16:45:56 -0500

    classcontroller action josn to coursecontroller actionview

--- urlinqyii/protected/controllers/CourseController.php
+++ urlinqyii/protected/controllers/CourseController.php
@@ -2,10 +2,6 @@
 
 class CourseController extends Controller
 {
-	public function actionView()
-	{
-		$this->render('view');
-	}
 
 	// Uncomment the following methods and override them if needed
 	/*
@@ -33,4 +29,62 @@ class CourseController extends Controller
 		);
 	}
 	*/
+
+    public function actionView() {
+        $course_id = $_GET['id'];
+        if($course_id == null) {
+            echo CJSON::encode(array(
+                'success' => false,
+                'message' => 'Invalid Course Id'
+            ));
+            Yii::app()->end();
+        }
+
+        $sql = "SELECT c.class_id, c.course_id, c.section_id, cr.course_name, c.department_id, d.department_name, c.component, c.color_id, c.location,
+              u.firstname as prof_fname, u.lastname as prof_lname, f1.file_url AS cover_file_url, f2.file_url AS picture_file_url
+            FROM class as c
+            INNER JOIN course as cr
+              ON c.course_id = cr.course_id
+            LEFT JOIN department as d
+              ON c.department_id = d.department_id
+            LEFT JOIN user AS u
+              ON c.professor = u.user_id
+            LEFT JOIN file AS f1
+              ON c.cover_file_id = f1.file_id
+            LEFT JOIN file AS f2
+              ON c.picture_file_id = f2.file_id
+            WHERE c.course_id = $course_id
+        ";
+
+//        , s.day, s.start_time, s.end_time
+        $classes = Yii::app()->db->createCommand($sql)->queryAll();
+        $schedules = array();
+        for($i=0; $i< count($classes); $i++)  {
+            $sql = "SELECT * FROM class_schedule as cs
+                      LEFT JOIN schedule AS s
+                        ON cs.schedule_id = s.schedule_id
+                        WHERE cs.class_id = {$classes[$i]['class_id']}";
+            $classes[$i]['schedule'] = array(
+                'day' => 'M',
+                'start_time' => '17:00:00',
+                'end_time' => '17:00:00'
+            );
+//            $classes[$i]['schedule'] = Yii::app()->db->createCommand($sql)->queryAll();
+
+
+            $sql = "SELECT * FROM class_user as cu
+                      LEFT JOIN user AS u
+                        ON cu.user_id = u.user_id
+                        WHERE cu.class_id = {$classes[$i]['class_id']}";
+            $classes[$i]['users'] = Yii::app()->db->createCommand($sql)->queryAll();
+
+        }
+        echo CJSON::encode(array(
+            'success' => true,
+            'count' => count($classes),
+            'data' => $classes
+        ));
+        Yii::app()->end();
+    }
+
 }
