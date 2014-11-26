c68731316165fa78cd81be0a35cc9a8c2c5c7853
Alex <adroitcode>
2014-11-25 19:59:48 -0500

    please do not reference beta files from urlinqyii. Organize css and js files into folders with descriptive names

--- urlinqyii/protected/views/partial/leftmenu.php
+++ urlinqyii/protected/views/partial/leftmenu.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/partial/leftmenu.css">    
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/partial/leftpanel/leftmenu.css">
     <link type="text/css" href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/jquery.jscrollpane.css" rel="stylesheet" media="all"/>
     <!--<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
     <script type="text/javascript" src="js/jquery.mousewheel.js"></script>-->
