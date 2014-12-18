6183522470d71bc588ecf29c5b75bafbce667140
Alex <adroitcode>
2014-12-18 13:41:10 -0500

    syncing

--- urlinqyii/js/partial/nav_bar/nav_bar.js
+++ urlinqyii/js/partial/nav_bar/nav_bar.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
 
+
     $('.nav_section').click(function(){
         var $this_section = $(this);
-        window.location.replace(base_url + $this_section.attr('data-link_url'));
+
+        //If this nav section is a dropdown, show the drop_down container
+        if($this_section.hasClass('drop_down')){
+            var $drop_down = $('#nav_drop_down_container');
+
+            if($drop_down.css('display') == 'none'){
+                $drop_down.show();
+            } else {
+                $drop_down.hide();
+            }
+        }else{
+            window.location.replace(base_url + $this_section.attr('data-link_url'));
+        }
+
+
     });
+
+
+
 
 
     $('.nav_arrow').click(function(){
