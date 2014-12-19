991e9fe87884ec4beac8620fc7203601303cad2d
Alex <adroitcode>
2014-12-19 17:21:39 -0500

    added php to leftpanel. syncing

--- urlinqyii/protected/views/partial/leftpanel.php
+++ urlinqyii/protected/views/partial/leftpanel.php
@@ -6,7 +6,6 @@
     <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/scroll/jquery.mCustomScrollbar.concat.min.js"></script>
     <link href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/jquery.mCustomScrollbar.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/partial/leftpanel/leftpanel.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/partial/leftpanel/leftpanel2.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
@@ -30,13 +29,13 @@
 				<div class = "LeftPanel_MyBox">
 					<div class = "clearfix MyBox">
 						<a class = "MyBox_PictureLink">
-							<img class = "MyBox_Picture" src="<?php echo Yii::app()->getBaseUrl(true); ?>/assets/LazDisplayPic.png">
+							<img class = "MyBox_Picture" src="<?php echo Yii::app()->getBaseUrl(true) . $user->pictureFile->file_url; ?>">
 						</a>
 						<div class = "MyBox_text">
 							<div class = "MyBox_textcontent">
 								<div class = "MyBox_NameSO">
 									<a class = "MyBox_ProfileLink">
-										Jacob Lazarus
+										<?php echo $user->firstname . " " . $user->lastname?>
 									</a>
 									<a class = "MyBox_SO" href="<?php echo Yii::app()->getBaseUrl(true); ?>/logout">
 										Sign out
@@ -51,7 +50,7 @@
 						<div class = "LeftPanel_DSContentBoxHeader">
 							<b>Department</b>
 						</div>
-						<a class = "LeftPanel_DSContentBoxName" href="<?php echo $user->department->department_id; ?>">
+						<a class = "LeftPanel_DSContentBoxName" href="<?php echo Yii::app()->getBaseUrl(true); ?>/department/<?php echo $user->department->department_id; ?>">
 							<h5><?php echo $user->department->department_name; ?></h5>
 						</a>						
 					</div>
@@ -59,8 +58,8 @@
 						<div class = "LeftPanel_DSContentBoxHeader">
 							<b>School</b>
 						</div>
-						<a class = "LeftPanel_DSContentBoxName">
-							<h5>NYU School of Engineering</h5>
+						<a class = "LeftPanel_DSContentBoxName" href="<?php echo Yii::app()->getBaseUrl(true); ?>/school/<?php echo $user->school->school_id; ?>">
+							<h5><?php echo $user->school->school_name; ?></h5>
 						</a>						
 					</div>					
 				</div>
@@ -81,21 +80,11 @@
 			</div>
 			<div class = "LeftPanel_SectionContent">
 				<ul class = "LeftPanel_GroupsList">
-					<li>
-						<a>Theories of the French Republic and Beorafa Part 2</a>
-					</li>
-					<li>
-						<a>Systems and Motor Neuroscience</a>
-					</li>
-					<li>
-						<a>Technical Entrepreneurship</a>
-					</li>		
-					<li>
-						<a>Web Programming</a>
-					</li>
-					<li>
-						<a>Principles of Economics</a>
-					</li>			
+                    <?php foreach($user->classes as $class){?>
+                        <li>
+                            <a href="<?php echo Yii::app()->getBaseUrl(true) . '/class/' . $class->class_id; ?>"><?php echo $class->class_name; ?></a>
+                        </li>
+                    <?php } ?>
 				</ul>
 			</div>						
 		</div>
@@ -114,66 +103,19 @@
 			</div>	
 			<div class = "LeftPanel_SectionContent">
 				<ul class = "LeftPanel_GroupsList">
-					<li>
-						<a>NYU Cheese Club</a>
-					</li>
-					<li>
-						<a>Gallatin Business Club</a>
-					</li>
-					<li>
-						<a>NYU Poly Hackathons</a>
-					</li>		
-					<li>
-						<a>Tech@NYU</a>
-					</li>		
+                    <?php foreach($user->groups as $club){ ?>
+
+
+                        <li>
+                            <a href="<?php echo Yii::app()->getBaseUrl(true) . '/club/' . $club->group_id; ?>"><?php echo $club->group_name; ?></a>
+                        </li>
+                    <?php } ?>
 				</ul>				
 			</div>					
 		</div>
 	</div>
 </div>
 
-<script>
-    (function($){
-        $(function () {
-            $(".leftpanel .scrollable .title").click(function () {
-                var title = $(this);
-                var clubs = $(".leftpanel .scrollable.club");
-                var classes = $(".leftpanel .scrollable.class");
-                (title.parents(".scrollable")[0] == classes[0] ? function () {
-                    //classes.toggleClass("min");
-                    // case 1: 50 50
-                    if (!(classes.hasClass("full") || classes.hasClass("min"))) {
-                        classes.addClass("min");
-                        clubs.addClass("full");
-                    } else if (classes.hasClass("min")) {
-                        if (clubs.hasClass("min")) {
-                            classes.removeClass("min").addClass("full");
-                        } else {
-                            classes.removeClass("min");
-                            clubs.removeClass("full");
-                        }
-                    } else if (classes.hasClass("full")) {
-                        classes.removeClass("full").addClass("min");
-                    }
-                } : function () {
-                    if (!(clubs.hasClass("full") || clubs.hasClass("min"))) {
-                        clubs.addClass("min");
-                        classes.addClass("full");
-                    } else if (clubs.hasClass("min")) {
-                        if (classes.hasClass("min")) {
-                            clubs.removeClass("min").addClass("full");
-                        } else {
-                            clubs.removeClass("min");
-                            classes.removeClass("full");
-                        }
-                    } else if (clubs.hasClass("full")) {
-                        clubs.removeClass("full").addClass("min");
-                    }
-                })();
-            });
 
-        })
-    })(jQuery);
-</script>
 </body>
 </html>
