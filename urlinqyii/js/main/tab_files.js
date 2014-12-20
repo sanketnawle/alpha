636a8636c492deb60848d829d3e6a2b63bd35d4c
Jakelaz <jacobmlazarus@gmail.com>
2014-12-20 17:13:27 -0500

    sycning file tab

--- urlinqyii/js/main/tab_files.js
+++ urlinqyii/js/main/tab_files.js
@@ -1,4 +1,15 @@
 
 $(document).ready(function(){
+	$('.files_subtab').click(function(){
+        var $tab = $(this);
+        var panel_id = $tab.attr('data-panel_id');
+        //Change active tab
+        $('.files_subtab.active').removeClass('active');
+        $tab.addClass('active');
 
+
+        //Find the current active panel and remove its active class
+        $('.files_sub_panel.active').removeClass('active');
+        $('#files_sub_panel_' + panel_id).addClass('active');
+    });
 });
