70ddb4aeb4dae3f5552bb9d2fb6ca6fa68748ed1
Alex <adroitcode>
2015-01-26 03:43:12 -0500

    .

--- urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
+++ urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
@@ -7,10 +7,10 @@ if (ERunActions::runBackground()){
     $to_email = $params['to_email'];
     $subject = $params['subject'];
     $from_email = $params['from_email'];
-    $user_firstname = 'Alex';
+
     $message = $params['message'];
 
-    $mail = new YiiMailer('confirmation', array('key'=>$key, 'user_firstname'=>$user_firstname, 'message'=>$message));
+    $mail = new YiiMailer('confirmation', array('key'=>$key, 'message'=>$message));
     $mail->setFrom($from_email, 'urlinq team');
     $mail->setSubject($subject);
     $mail->setTo($to_email);
