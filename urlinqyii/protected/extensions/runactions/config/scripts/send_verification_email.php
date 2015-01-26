29c69a66f54654077927d68285bedc8570dc1cf6
Alex <adroitcode>
2015-01-26 14:47:30 -0500

    syncing

--- urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
+++ urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
@@ -12,20 +12,39 @@ if (ERunActions::runBackground()){
 
     $mail = new YiiMailer('confirmation', array('key'=>$key, 'message'=>$message));
 
-
-
-    ini_set('sendmail_from', 'team@urlinq.com');
+//    $mail->He
+//
+//    $emailto = 'to@domain.com';
+//    $toname = 'TO NAME';
+//    $emailfrom = 'from@domain.com';
+//    $fromname = 'FROM NAME';
+//    $subject = 'Email Subject';
+//    $messagebody = 'Hello.';
+//    $headers =
+//        'Return-Path: ' . $emailfrom . "\r\n" .
+//        'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+//        'X-Priority: 3' . "\r\n" .
+//        'X-Mailer: PHP ' . phpversion() .  "\r\n" .
+//        'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+//        'MIME-Version: 1.0' . "\r\n" .
+//        'Content-Transfer-Encoding: 8bit' . "\r\n" .
+//        'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+//    $params = '-f ' . $emailfrom;
+//    $test = mail($emailto, $subject, $messagebody, $headers, $params);
 
     $mail->Mailer='smtp';
+    $mail->Sender = "team@urlinq.com";
     $mail->setFrom($from_email, 'urlinq team');
     $mail->setSubject($subject);
     $mail->setTo($to_email);
+    $mail->SMTPDebug = 1;
 
-    $mail->set('username', "team@urlinq.com"); // SMTP username
+    $mail_response = $mail->send();
 
 
+    Yii::log($mail_response);
 
-    return $mail->send();
+    return $mail_response;
 }
 
 
