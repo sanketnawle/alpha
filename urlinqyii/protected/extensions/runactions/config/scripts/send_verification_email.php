48925b87f5a72ab59afebd4f82ce218f6d2d2386
Alex <adroitcode>
2015-01-26 15:20:42 -0500

    syncing

--- urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
+++ urlinqyii/protected/extensions/runactions/config/scripts/send_verification_email.php
@@ -45,38 +45,62 @@ if (ERunActions::runBackground()){
 //    $mail->SMTPSecure = 'ssl';
 //    $mail->SMTPDebug = 1;
 
-    $mail->Mailer='smtps';
-    $mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 1;  // debugging: 1 = errors and messages, 2 = messages only
-	$mail->SMTPAuth = true;  // authentication enabled
-	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-	$mail->Host = 'box791.bluehost.com';
-	$mail->Port = 465;
-	$mail->Username = 'team@urlinq.com';
-	$mail->Password = 'test#123';
-	$mail->SetFrom($from, 'Urlinq Team');
-	$mail->Subject = '';
-	$mail->Body = '';
-	$mail->AddAddress($to_email);
+//    $mail->Mailer='smtps';
+//    $mail->IsSMTP(); // enable SMTP
+//	$mail->SMTPDebug = 1;  // debugging: 1 = errors and messages, 2 = messages only
+//	$mail->SMTPAuth = true;  // authentication enabled
+//	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+//	$mail->Host = 'box791.bluehost.com';
+//	$mail->Port = 465;
+//	$mail->Username = 'team@urlinq.com';
+//	$mail->Password = 'test#123';
+//	$mail->SetFrom($from, 'Urlinq Team');
+//	$mail->Subject = '';
+//	$mail->Body = '';
+//	$mail->AddAddress($to_email);
+//
+//
+//    $mail_response = $mail->send();
+//
+//
+//    Yii::log($mail_response);
+//
+//
+//    if($mail_response){
+//        return var_dump('EMAIL SENT');
+//    }
+//    else
+//    {
+//        echo '<pre>';
+//        var_dump($mail->getError());
+//        echo '</pre>';
+//    }
+//
 
 
-    $mail_response = $mail->send();
+    $mail = new YiiMailer();
+    $mail->IsSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'box791.bluehost.com';  // Specify main and backup server
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'team@urlinq.com';                            // SMTP username
+    $mail->Password = 'test#123';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+    $mail->Port = 587;
+                //set mail properties
+    $mail->setFrom($from_email, 'YYY');
+
+    $mail->setTo($to_email);
+
+    $mail->setSubject('');
+    //$mail->setBody($model->message, 'text/html');
+
+    return $mail->send();
 
 
-    Yii::log($mail_response);
 
 
-    if($mail_response){
-        return var_dump('EMAIL SENT');
-    }
-    else
-    {
-        echo '<pre>';
-        var_dump($mail->getError());
-        echo '</pre>';
-    }
 
-    return $mail_response;
+     //$mail_response;
 }
 
 
