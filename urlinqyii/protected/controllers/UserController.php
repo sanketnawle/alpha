f7286a7f7cfe1d23e65f300fbb813cca560baf13
Ben Rosen <benrosen78@gmail.com>
2015-02-01 15:50:24 -0600

    forgot a space :(

--- urlinqyii/protected/controllers/UserController.php
+++ urlinqyii/protected/controllers/UserController.php
@@ -338,7 +338,7 @@ class UserController extends Controller
     //            return;
 
             if ($_GET['last_notification_id']) {
-                $notifications = Notification::model()->findAllBySql('SELECT * FROM `notification` WHERE user_id = ' . $user->user_id . 'AND notification_id < ' . $_GET['last_notification_id'] . ' ORDER BY notification_id DESC limit 10');
+                $notifications = Notification::model()->findAllBySql('SELECT * FROM `notification` WHERE user_id = ' . $user->user_id . ' AND notification_id < ' . $_GET['last_notification_id'] . ' ORDER BY notification_id DESC limit 10');
             } else {
                 $notifications = Notification::model()->findAllBySql('SELECT * FROM `notification` WHERE user_id = ' . $user->user_id . ' ORDER BY notification_id DESC limit 5');
             }
