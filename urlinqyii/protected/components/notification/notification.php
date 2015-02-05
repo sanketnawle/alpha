41ede8602d79406169591fb840013233de2c3127
Ben Rosen <benrosen78@gmail.com>
2015-02-05 17:58:47 -0600

    trying to fix push notis

--- urlinqyii/protected/components/notification/notification.php
+++ urlinqyii/protected/components/notification/notification.php
@@ -10,12 +10,14 @@ function send_notification($notification_type, $actor_id, $user_id, $origin_id, 
 
     $notification->save(false);
 
-//This is breaking shit
-//
-//
-//    include_once 'iOSPushNotifications.php';
-//    $user = User::model()->find('user_id=:id', array(':id'=>$notification->user_id));
-//    notifyAlliOSDevicesForUserID($user_id, get_notification_text($notification, $user));
+try {
+
+
+    include_once 'iOSPushNotifications.php';
+    $user = User::model()->find('user_id=:id', array(':id'=>$notification->user_id));
+    notifyAlliOSDevicesForUserID($user_id, get_notification_text($notification, $user));
+
+}
 }
 
 function get_notification_text($noti, $user) {
