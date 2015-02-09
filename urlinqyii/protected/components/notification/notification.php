75aa207145ef940e7dd064448de4e4448187a888
Ben Rosen <benrosen78@gmail.com>
2015-02-09 17:15:49 -0600

    work

--- urlinqyii/protected/components/notification/notification.php
+++ urlinqyii/protected/components/notification/notification.php
@@ -13,7 +13,7 @@ function send_notification($notification_type, $actor_id, $user_id, $origin_id, 
     include_once 'iOSPushNotifications.php';
     $notification = Notification::model()->find('notification_id=:id', array(':id'=>$notification->notification_id));
     $user = User::model()->find('user_id=:id', array(':id'=>$user_id));
-    $notification_array = array_for_notification_model($notification);
+    $notification_array = $this->array_for_notification_model($notification);
   //  $message = get_notification_text($notification_array, $user);
 
 $thing = get_notifications_data($user, array($notification_array));
