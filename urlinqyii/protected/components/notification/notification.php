bd09f2118c9e87a945e7d2e2be70a230acdd617c
Ben Rosen <benrosen78@gmail.com>
2015-02-09 16:41:02 -0600

    found the culprit

--- urlinqyii/protected/components/notification/notification.php
+++ urlinqyii/protected/components/notification/notification.php
@@ -16,7 +16,7 @@ function send_notification($notification_type, $actor_id, $user_id, $origin_id, 
     $notification_array = array_for_notification_model($notification);
     //$message = get_notification_text($notification_array, $user);
 
-$thing = get_notification_data($user, $notification_array);
+//$thing = get_notification_data($user, $notification_array);
 
     notifyAlliOSDevicesForUserID($user_id, "testtest");
 
