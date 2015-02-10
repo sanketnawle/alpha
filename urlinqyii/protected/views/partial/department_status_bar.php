aa3d476e107fde7c4ba34fb59f2c53c4e933cefe
Michael Ohr <mohr24@gmail.com>
2015-02-10 15:18:47 -0500

    dept and school status bar. change profile picture.

--- urlinqyii/protected/views/partial/department_status_bar.php
+++ urlinqyii/protected/views/partial/department_status_bar.php
@@ -70,7 +70,7 @@
             <form id = "fbar_form">
                 <div class = "form_wrapper">
                     <header id = "fbar_header" class = "fbar_contents_fix">
-                        <a class = "audience_default"><div id = "audience_select"><span>To <span class = "selected_audience">Department Name</span></span></div></a>
+                        <a class = "audience_default"><div id = "audience_select"><span>To <span class = "selected_audience"><?php echo $origin->department_name;?></span></span></div></a>
                         <div id = "discussion_form_content" class = "post_type_header active post_type_discussion"><span>Post</span></div>  
                         <div id = "event_form_content" class = "post_type_header active post_type_events"><span>Event</span></div> 
                         <div id = "opportunity_form_content" class = "post_type_header active post_type_opportunity"><span>Opportunity - Job/Research/Project</span></div>                 
