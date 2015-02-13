6e42be5cec85d8f41086abed49651334a6bed178
Jakelaz <jacobmlazarus@gmail.com>
2015-02-13 00:28:11 -0500

    syncing

--- urlinqyii/protected/views/partial/planner.php
+++ urlinqyii/protected/views/partial/planner.php
@@ -17,14 +17,7 @@
 		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
         <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/module/timezone_conversion.js"></script>
-<!--		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
-<!--		<script src="jquery-ui-1.11.0/jquery-ui.min.js"></script>-->
 
-<!--        <script src="//code.jquery.com/jquery-1.10.2.js"></script>-->
-<!---->
-<!--        <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>-->
-
-<!--        <script src="--><?php //echo Yii::app()->getBaseUrl(true); ?><!--/js/jquery.min.js" type="text/javascript"></script>-->
 
         <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/jquery-ui-1.10.2.custom.min.js"></script>
         <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/calendar_selector.js" type="text/javascript"></script>
@@ -38,15 +31,6 @@
         <script type="text/javascript" src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/handlebars.js" > </script>
 
 
-<!--
-        <script src="<?php// echo Yii::app()->getBaseUrl(true); ?>/js/time_selector/time_selector.js"></script>
-        <link href="<?php// echo Yii::app()->getBaseUrl(true); ?>/css/time_selector/time_selector.css" type = "text/css" rel = "stylesheet">
-
-
-        <script src="<?php// echo Yii::app()->getBaseUrl(true); ?>/js/date_selector/date_selector.js" type="text/javascript"></script>
-    -->
-        <!--old planner.js file, I leave it here for your reference-->
-        <!--<script src="js/planner.js" type="text/javascript"></script>-->
 	</head>
 	<body>
 		<div class="planner_container">
