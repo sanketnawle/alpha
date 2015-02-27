7be2df79ed13e004e2ef3f498135ca7a0642b489
Afroze Khan <afroze.vy349@gmail.com>
2015-02-27 00:01:02 -0500

    Loading animation chnaged

--- urlinqyii/protected/views/site/home.php
+++ urlinqyii/protected/views/site/home.php
@@ -61,7 +61,6 @@
         <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/date_selector/date_selector.js" type="text/javascript"></script>
         <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/lightbox.min.js" type="text/javascript"></script>
         <link href="<?php echo Yii::app()->getBaseUrl(true); ?>/css/lightbox.css" type = "text/css" rel = "stylesheet">
-        <script src="<?php echo Yii::app()->getBaseUrl(true); ?>/js/iframe_script.js" type="text/javascript"></script>
  
     </head>
 
