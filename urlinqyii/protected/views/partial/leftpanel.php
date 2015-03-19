6c5c81fa4208c460b6efa7e31b043c1b1a7b1d65
adroitcode <=>
2015-03-18 20:11:43 -0400

    zetabetatau/msging

--- urlinqyii/protected/views/partial/leftpanel.php
+++ urlinqyii/protected/views/partial/leftpanel.php
@@ -134,8 +134,7 @@ if($user->school->university_id == 4){
 		</div>
 
 
-<!--        --><?php //echo $this->renderPartial('/partial/messaging_panel',array('user'=>$user,'origin_type'=>'club','origin_id'=>$club->group_id,'origin_name'=>$club->group_name)); ?>
-
+        <?php echo $this->renderPartial('/partial/messaging_panel',array('user'=>$user,'origin_type'=>$origin_type,'origin_id'=>$origin_id)); ?>
 
 
 
