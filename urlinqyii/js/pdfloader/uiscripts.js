5e5aa1be01dc60c228b4a5efc60fd42046778d56
Afroze Khan <afroze.vy349@gmail.com>
2015-03-22 23:21:49 -0400

    Safari fix for the syllabus tab

--- urlinqyii/js/pdfloader/uiscripts.js
+++ urlinqyii/js/pdfloader/uiscripts.js
@@ -149,8 +149,12 @@ var load_events = function (pdf_id) {
          data: {"class_id":globals.origin_id, "file_id":pdf_id},
          success: function(response) {
           $.each(response, function(index,value){
-                var d = new Date(value["start_date"]+" "+ value["start_time"]);
-                var dt = new Date(value["end_date"]+" "+ value["end_time"]);
+                start_date_split = value["start_date"].split("-");
+                end_date_split = value["end_date"].split("-");
+                start_date_str = start_date_split[1]+"/"+start_date_split[2]+"/"+start_date_split[0];
+                end_date_str = end_date_split[1]+"/"+end_date_split[2]+"/"+end_date_split[0];
+                var d = new Date(start_date_str+" "+ value["start_time"]);
+                var dt = new Date(end_date_str+" "+ value["end_time"]);
                 if(d.getHours()!="0" && dt.getHours()!="0"){
                   time = "from "+formatAMPM(d)+" to "+formatAMPM(dt);
                 }
@@ -182,7 +186,7 @@ var load_events = function (pdf_id) {
                   month: month[d.getMonth()],
                   event_id: value["event_id"],
                   time: time,
-                  color: class_color,
+                  color: get_class_color(),
                   origin_type: value["event_type"],
                 };
                   event_array_list.push(event_value);
@@ -412,7 +416,7 @@ var $chip = $(this);
                     </div>\
                   <div class="people-attending">'+get_people_attending(form_data["event_id"])+'</div>\
                 </div>\
-                <div class="card-upload">Materials <button id="btn_event_file_upload">Upload</button><button>Import from drive</button></div>';
+                <div class="card-upload">Materials <button id="btn_event_file_upload">Upload</button><input type="file" id="event_file_upload" style="display:none"/><button>Import from drive</button></div>';
     card_content = $(card_content_temp).html(card_html);
 
     if (!$($chip).hasClass('expanded')) {
@@ -549,11 +553,10 @@ $(document).on("click", "#btn_update_location", function(event){
 
 $(document).on("click",'#btn_event_file_upload', function(event){
         event.preventDefault();
-        
         $("#event_file_upload").trigger("click");
 });
 
-$(document).on("change",'#event_file_upload', function(event){
+$(document).on("change",'.event_file_upload', function(event){
         event.preventDefault();
 
         filevalue = $(this).val().split(".");
@@ -698,7 +701,7 @@ var search_events = function(){
                       month: month[d.getMonth()],
                       event_id: value["event_id"],
                       time: time,
-                      color: class_color,
+                      color: get_class_color(),
                       origin_type: value["origin_type"],
                     };
                       event_array_list.push(event_value);
@@ -720,7 +723,6 @@ var search_events = function(){
 };
 
 function get_people_attending(event_id){
-  console.log(event_id);
   var people_attending_html = "";
  var resp = $.ajax({
                url: "GetPeopleAttending",
